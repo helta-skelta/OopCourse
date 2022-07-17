95d30d8f06952d3bc019c3f7c4c6ec3bfcc388b0
helta-skelta <fobevc@mail.ru>
2022-07-17 12:51:40 +0700

    Done

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа диапазона.");
+
+Console.Write("Начало диапазона: ");
+double from = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Конец диапазона: ");
+double to = Convert.ToDouble(Console.ReadLine());
+
+RangeTask.Range range = new(from, to);
+
+Console.WriteLine("Длина диапазона: " + range.GetLength());
+
+Console.Write("Введите число для проверки принадлежности данному диапазону: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (range.IsInside(number))
+{
+    Console.WriteLine($"Число {number} лежит в диапазоне от {from} до {to}.");
+}
+else
+{
+    Console.WriteLine($"Число {number} не лежит в диапазоне от {from} до {to}.");
+}
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace RangeTask
 {
-    internal class Range
+    class Range
     {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
     }
 }
