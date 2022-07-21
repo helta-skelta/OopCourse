8e6343f06405577e5fce18c1f78ced9ca310ba4a
helta-skelta <fobevc@mail.ru>
2022-07-21 16:28:57 +0700

    Range*

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,23 +1,86 @@
-﻿Console.WriteLine("Введите два числа диапазона.");
+﻿using Range = RangeTask.Range;
 
-Console.Write("Начало диапазона: ");
-double from = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите начало и конец двух диапазонов чисел.");
 
-Console.Write("Конец диапазона: ");
-double to = Convert.ToDouble(Console.ReadLine());
+Console.Write("Начало первого диапазона: ");
+double from1 = Convert.ToDouble(Console.ReadLine());
 
-RangeTask.Range range = new(from, to);
+Console.Write("Конец первого диапазона: ");
+double to1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Длина диапазона: " + range.GetLength());
+Range range1 = new(from1, to1);
 
-Console.Write("Введите число для проверки принадлежности данному диапазону: ");
-double number = Convert.ToDouble(Console.ReadLine());
+Console.Write("Начало вторго диапазона: ");
+double from2 = Convert.ToDouble(Console.ReadLine());
 
-if (range.IsInside(number))
+Console.Write("Конец второго диапазона: ");
+double to2 = Convert.ToDouble(Console.ReadLine());
+
+Range range2 = new(from2, to2);
+
+Range? intervalsIntersection = new();
+intervalsIntersection = intervalsIntersection.GetIntervalsIntersection(range1, range2);
+
+Console.WriteLine();
+Console.WriteLine("Пересечение интервалов.");
+Console.WriteLine();
+
+if (intervalsIntersection == null)
 {
-    Console.WriteLine($"Число {number} лежит в диапазоне от {from} до {to}.");
+    Console.WriteLine("null");
 }
 else
 {
-    Console.WriteLine($"Число {number} не лежит в диапазоне от {from} до {to}.");
+    Console.WriteLine($"Длинна интервала-пересечения = {intervalsIntersection.GetLength()}.");
+    Console.WriteLine($"Начало интервала-пересечения = {intervalsIntersection.From}. Конец интервала-пересечения = {intervalsIntersection.To}.");
+}
+
+Range[] intervalsJoining = range1.GetIntervalsJoining(range1, range2);
+
+Console.WriteLine();
+Console.WriteLine("Обьединение интервалов.");
+Console.WriteLine();
+
+if (intervalsJoining == null)
+{
+    Console.WriteLine("null");
+}
+else
+{
+    if (intervalsJoining.Length == 1)
+    {
+        Console.WriteLine($"Длинна обьединенных интервалов = {intervalsJoining[0].GetLength()}");
+        Console.WriteLine($"Начало обьединенного интервала = {intervalsJoining[0].From}. Конец обьединенного интервала = {intervalsJoining[0].To}.");
+    }
+    else
+    {
+        Console.WriteLine($"Длинна обьединенных интервалов = {intervalsJoining[0].GetLength() + intervalsJoining[1].GetLength()}.");
+        Console.WriteLine($"Начало первого интервала = {intervalsJoining[0].From}. Конец первого интервала = {intervalsJoining[0].To}.");
+        Console.WriteLine($"Начало второго интервала = {intervalsJoining[1].From}. Конец второго интервала = {intervalsJoining[1].To}.");
+    }
+}
+
+Range[]? intervalsDifference = range1.GetIntervalsDifference(range1, range2);
+
+Console.WriteLine();
+Console.WriteLine("Разность интервалов.");
+Console.WriteLine();
+
+if (intervalsDifference == null)
+{
+    Console.WriteLine("null");
+}
+else
+{
+    if (intervalsDifference.Length == 1)
+    {
+        Console.WriteLine($"Длинна интервала = {intervalsDifference[0].GetLength()}.");
+        Console.WriteLine($"Начало интервала = {intervalsDifference[0].From}. Конец интервала = {intervalsDifference[0].To}.");
+    }
+    else
+    {
+        Console.WriteLine($"Длинна интервала = {intervalsDifference[0].GetLength() + intervalsDifference[1].GetLength()}");
+        Console.WriteLine($"Начало первого интервала = {intervalsDifference[0].From}. Конец первого интервала = {intervalsDifference[0].To}.");
+        Console.WriteLine($"Начало второго интервала = {intervalsDifference[1].From}. Конец второго интервала = {intervalsDifference[1].To}.");
+    }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -8,10 +8,15 @@ namespace RangeTask
 
         public double To { get; set; }
 
-        public Range(double from, double to )
+        public Range(double from, double to)
         {
             From = from;
             To = to;
+        }
+
+        public Range()
+        {
+
         }
 
         public double GetLength()
@@ -22,6 +27,111 @@ namespace RangeTask
         public bool IsInside(double number)
         {
             return number >= From && number <= To;
+        }
+
+        public Range? GetIntervalsIntersection(Range interval1, Range interval2)
+        {
+            if (interval1.From >= interval2.From && interval1.To <= interval2.To)
+            {
+                Range result = new(interval1.From, interval1.To);
+
+                return result;
+            }
+
+            if (interval2.From >= interval1.From && interval2.To <= interval1.To)
+            {
+                Range result = new(interval2.From, interval2.To);
+
+                return result;
+            }
+
+            if (interval1.To > interval2.From && interval1.From <= interval2.From)
+            {
+                Range result = new(interval2.From, interval1.To);
+
+                return result;
+            }
+
+            if (interval1.To >= interval2.To && interval1.From < interval2.To)
+            {
+                Range result = new(interval1.From, interval2.To);
+
+                return result;
+            }
+
+            return null;
+        }
+
+        public Range[] GetIntervalsJoining(Range interval1, Range interval2)
+        {
+            if (interval1.To > interval2.From && interval1.From <= interval2.From)
+            {
+                Range newInterval = new(interval1.From, interval2.To);
+
+                Range[] result = { newInterval };
+
+                return result;
+            }
+
+            if (interval2.To > interval1.From && interval2.From <= interval1.From)
+            {
+                Range newInterval = new(interval2.From, interval1.To);
+
+                Range[] result = { newInterval };
+
+                return result;
+            }
+
+            Range[] _result = { interval1, interval2 };
+
+            return _result;
+        }
+
+        public Range[]? GetIntervalsDifference(Range interval1, Range interval2)
+        {
+            if (interval1.From > interval2.From && interval1.To < interval2.To)
+            {
+                Range newInterval1 = new(interval2.From, interval1.From);
+                Range newInterval2 = new(interval1.To, interval2.To);
+
+                Range[] result = { newInterval1, newInterval2 };
+
+                return result;
+            }
+
+            if (interval2.From > interval1.From && interval2.To < interval1.To)
+            {
+                Range newInterval1 = new(interval1.From, interval2.From);
+                Range newInterval2 = new(interval2.To, interval1.To);
+
+                Range[] result = { newInterval1, newInterval2 };
+
+                return result;
+            }
+
+            if (interval1.To > interval2.From && interval1.From <= interval2.From)
+            {
+                Range newInterval = new(interval1.From, interval2.From);
+
+                Range[] result = { newInterval };
+
+                return result;
+            }
+
+            if (interval2.To > interval1.From && interval2.From <= interval1.To)
+            {
+                Range newInterval = new(interval2.To, interval1.To);
+
+                Range[] result = { newInterval };
+
+                return result;
+            }
+
+            Range _newInterval = new(0, 0);
+
+            Range[] _result = { _newInterval };
+
+            return _result;
         }
     }
 }
