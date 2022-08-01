b9be8e5b27dabe99f2a4c91799ba5295833e5aab
helta-skelta <fobevc@mail.ru>
2022-08-01 10:51:46 +0700

    VectorTask

---
+++ VectorTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using VectorTask;
+
+double[] components1 = { 3, 5, 6, 7 };
+double[] components2 = { 1, 2, 3, 8, 9, 10, -1 };
+
+Vector a = new(components1);
+Vector b = new(10, components2);
+Vector c = new(a);
+Vector d = new(7);
+
+d.Joining(b);
+Console.WriteLine("Прибавили к вектору \"d\" вектор \"b\" = " + d);
+
+Console.WriteLine("Размер вектора \"d\" = " + d.GetSize());
+Console.WriteLine("Длинна вектора \"d\" = " + d.GetLength());
+Console.WriteLine("Компонента вектора \"d\" под индексом \"3\" = " + d[3]);
+Console.WriteLine();
+
+d[3] = 6;//Установили компоненту вектора "d" под индексом 3
+
+d.Subtraction(a);
+Console.WriteLine("Вычли из вектора \"d\" вектор \"a\" = " + d);
+
+c.ScalarMultiply(2.0);
+Console.WriteLine("Умножили вектор \"c\" на 2 = " + c);
+
+c.Reverse();
+Console.WriteLine("Разворот вектора \"c\"  = " + c);
+
+if (c == b)
+{
+    Console.WriteLine("Вектор \"с\" равен вектору \"b\"");
+}
+else
+{
+    Console.WriteLine("Вектор \"с\" не равен вектору \"b\"");
+}
+
+Console.WriteLine("Хэш-код вектора \"c\" = " + c.GetHashCode());
+Console.WriteLine("Хэш-код вектора \"b\" = " + b.GetHashCode());
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine("Разность векторов \"a\" и \"b\" = " + Vector.Subtract(a, b));
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine("Сумма векторов \"a\" и \"b\" = " + Vector.Join(a, b));
+Console.WriteLine("Разность векторов \"a\" и \"b\" = " + Vector.Subtract(a, b));
+Console.WriteLine("Произведение векторов \"a\" и \"b\" = " + Vector.Multiply(a, b));
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,214 @@
+﻿namespace VectorTask
+{
+    internal class Vector
+    {
+        private double[]? components;
+
+        public double[]? Components
+        {
+            get => components;
+            set => components = value is null || value.Length == 0 ? throw new ArgumentNullException(nameof(value), "Vector is null or void") : value;
+        }
+
+        public double this[int index]
+        {
+            get => components![index];
+            set => components![index] = value;
+        }
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Длинна массива должна быть > 0", nameof(n));
+            }
+
+            Components = new double[n];
+        }
+
+        public Vector(Vector vector) : this(vector!.Components!.Length, vector.Components) { }
+
+        public Vector(double[] components) : this(components.Length, components) { }
+
+        public Vector(int n, double[] components) : this(n)
+        {
+            for (int i = 0; i < components?.Length; ++i)
+            {
+                Components![i] = components[i];
+            }
+        }
+
+        public int GetSize() => Components!.Length;
+
+        public override string ToString()
+        {
+            return $"{{{String.Join(", ", Components!)}}}";
+        }
+
+        public void Joining(Vector vector)
+        {
+            if (vector is null)
+            {
+                throw new ArgumentNullException(nameof(vector), "Vector is null");
+            }
+
+            if (components!.Length < vector!.components!.Length)
+            {
+                Array.Resize(ref this.components, vector!.components!.Length);
+            }
+            else if (components!.Length < vector!.components!.Length)
+            {
+                Array.Resize(ref vector!.components, components!.Length);
+            }
+
+            for (int i = 0; i < components!.Length; ++i)
+            {
+                components![i] += vector!.components![i];
+            }
+        }
+
+        public void Subtraction(Vector vector)
+        {
+            if (vector is null)
+            {
+                return;
+            }
+
+            if (vector!.components!.Length <= components!.Length)
+            {
+                for (int i = 0; i < vector.components!.Length; ++i)
+                {
+                    components[i] -= vector.components![i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < components!.Length; ++i)
+                {
+                    components[i] -= vector.components![i];
+                }
+            }
+        }
+
+        public void ScalarMultiply(double factor)
+        {
+            for (int i = 0; i < components!.Length; ++i)
+            {
+                components[i] *= factor;
+            }
+        }
+
+        public void Reverse()
+        {
+            ScalarMultiply(-1);
+        }
+
+        public double GetLength()
+        {
+            double length = 0;
+
+            for (int i = 0; i < components!.Length; ++i)
+            {
+                length += components[i] * components[i];
+            }
+
+            return Math.Sqrt(length);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Vector vector = (Vector)obj;
+
+            return ToString() == vector.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            int prime = 31;
+
+            hash = prime * hash + (components != null ? components.GetHashCode() : 0);
+
+            return hash;
+        }
+
+        public static Vector Join(Vector a, Vector b)
+        {
+            if (a is null)
+            {
+                throw new ArgumentNullException(nameof(a), "Vector is null");
+            }
+
+            if (b is null)
+            {
+                throw new ArgumentNullException(nameof(b), "Vector is null");
+            }
+
+            Vector result = new(a);
+
+            result.Joining(b);
+
+            return result;
+        }
+
+        public static Vector Subtract(Vector a, Vector b)
+        {
+            if (a is null)
+            {
+                throw new ArgumentNullException(nameof(a), "Vector is null");
+            }
+
+            if (b is null)
+            {
+                throw new ArgumentNullException(nameof(b), "Vector is null");
+            }
+
+            Vector result = new(a);
+
+            result.Subtraction(b);
+
+            return result;
+        }
+
+        public static Vector Multiply(Vector a, Vector b)
+        {
+            if (a is null)
+            {
+                throw new ArgumentNullException(nameof(a), "Vector is null");
+            }
+
+            if (b is null)
+            {
+                throw new ArgumentNullException(nameof(b), "Vector is null");
+            }
+
+            if (a.components!.Length < b.components!.Length)
+            {
+                Array.Resize(ref a.components, b.components!.Length);
+            }
+            else if (a.components.Length < b.components!.Length)
+            {
+                Array.Resize(ref b.components, a.components.Length);
+            }
+
+            Vector result = new(a);
+
+            for (int i = 0; i < a.components!.Length; ++i)
+            {
+                result[i] *= b[i];
+            }
+
+            return result;
+        }
+    }
+}
